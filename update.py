7dc764b227576fe29aa7d7a834457b3c486159e5
kulgg <75735874+kulgg@users.noreply.github.com>
2022-08-09 13:39:50 +0200

    Add class for files

--- update.py
+++ update.py
@@ -2,18 +2,24 @@ import os
 from pathlib import Path
 import shutil
 
+class DotFile:
+    def __init__(self, position, name):
+        self.position = position
+        self.name = name
+
 files = [
-            "Code/User/settings.json",
-            "i3/config",
-            "i3status/config"
+            DotFile(".config", "Code/User/settings.json"),
+            DotFile(".config", "i3/config"),
+            DotFile(".config", "i3status/config")
         ]
 
 def main():
     current_dir = os.getcwd()
     home_dir = Path.home()
 
+    print("Copying files")
     for f in files:
-        shutil.copyfile(Path(home_dir, ".config", f), Path(current_dir, f))
+        shutil.copyfile(Path(home_dir, f.position, f.name), Path(current_dir, f.name))
 
 if __name__ == '__main__':
     main()
