a59e24d6d99328a4139a92dbbfd7e57594fd06c0
kulgg <75735874+kulgg@users.noreply.github.com>
2022-08-09 14:10:45 +0200

    Replace . with dot

--- update.py
+++ update.py
@@ -2,16 +2,15 @@ import os
 from pathlib import Path
 import shutil
 
-class DotFile:
-    def __init__(self, position, name):
-        self.position = position
-        self.name = name
-
 files = [
-            DotFile(".config", "Code/User/settings.json"),
-            DotFile(".config", "i3/config"),
-            DotFile(".config", "i3status/config")
+            ".config/Code/User/settings.json",
+            ".config/i3/config",
+            ".config/i3status/config",
         ]
+
+
+def replace_dots(s):
+    return "/".join(s.split("/")[:-1]).replace(".", "dot")
 
 def main():
     current_dir = os.getcwd()
@@ -19,7 +18,12 @@ def main():
 
     print("Copying files")
     for f in files:
-        shutil.copyfile(Path(home_dir, f.position, f.name), Path(current_dir, f.name))
+        src_path = Path(home_dir, f)
+        destination_dir = replace_dots(f)
+        destination_file = src_path.name
+        dst_path = Path(current_dir, f"{destination_dir}/{destination_file}")
+        os.makedirs(destination_dir, exist_ok=True)
+        shutil.copyfile(src_path, dst_path)
 
 if __name__ == '__main__':
     main()
